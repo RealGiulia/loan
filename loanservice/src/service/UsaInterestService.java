b7f9de464e1f37b71211f2c38a6ab95c9cece6e9
Giulia Real <giuliaguerreiro9@gmail.com>
2021-11-24 10:45:35 -0300

    loan project created

---
+++ loanservice/src/service/UsaInterestService.java
@@ -0,0 +1,14 @@
+package service;
+
+public class UsaInterestService {
+	private double interestRate;
+
+	public UsaInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
